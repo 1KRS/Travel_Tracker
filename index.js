8399cb96e43a04590ab210e27fe251e88db1de53
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-11-08 14:40:30 +0100

    Update index.js
    Ενημερώστε τη μέθοδο .post για να προσθέσετε μια νέα επισκεφθείσα χώρα
    
    Update .post method in order to add a new visited country

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import bodyParser from 'body-parser';
 import pg from 'pg';
 import 'dotenv/config';
 
+// Σύνδεση σε Διακομιστή (Connect to Server)
 const διακ = express();
 const πύλη = 3000;
 
+// Σύνδεση σε Βάση Δεδομένων (Connect to Database)
 const db = new pg.Client({
   user: 'postgres',
   host: 'localhost',
@@ -17,31 +19,57 @@ const db = new pg.Client({
 db.connect();
 
 // Αρχικές Τιμές (Initial Values)
-var επισκεφθέντεςΧώρες = [];
-var σύνολοΧωρών = 0;
+let χώρες = []
+let επισκεφθείσεςΧώρες = [];
+let αριθμόςΧωρών = 0;
+let αριθμόςΕπισκεφθέντωνΧωρών = 0;
+
 
 // Δεδομένα (Data)
 // Λειτουργίες (Functions)
+const λήψηΧωρών = async (req, res, next) => {
+  const data = await db.query(`SELECT * FROM countries`)
+  χώρες = data.rows
+  αριθμόςΧωρών = data.rowCount
+  next();
+}
+
+const λήψηΕπισκεφθέντωνΧωρών = async (req, res, next) => {
+  const data = await db.query(`SELECT * FROM visited_countries`)
+  αριθμόςΕπισκεφθέντωνΧωρών = data.rowCount;
+  επισκεφθείσεςΧώρες = data.rows.map((row) => row.country_code);
+  next();
+}
+
 // Ενδιάμεσες Λειτουργίες (Middleware)
 διακ.use(bodyParser.urlencoded({ extended: true }));
 διακ.use(express.static('public'));
+διακ.use(λήψηΧωρών)
+διακ.use(λήψηΕπισκεφθέντωνΧωρών)
 
 // GET
 διακ.get('/', async (req, res) => {
-  await db.query(`SELECT * FROM visited_countries`, (err, res) => {
-    if (err) {
-      console.log('Πρόβλημα ολοκλήρωσης αναζήτησης', err.stack);
-    } else {
-      σύνολοΧωρών = res.rowCount
-      επισκεφθέντεςΧώρες = res.rows.map(row => row.country_code) 
-    }
+  console.log('Επισκεφθείσες Χώρες:', αριθμόςΕπισκεφθέντωνΧωρών, επισκεφθείσεςΧώρες);
+  res.render('index.ejs', {
+    total: αριθμόςΕπισκεφθέντωνΧωρών,
+    countries: επισκεφθείσεςΧώρες,
   });
-  console.log('Επισκεφθέντες Χώρες:', σύνολοΧωρών, επισκεφθέντεςΧώρες);
-  res.render('index.ejs', {total: σύνολοΧωρών, countries: επισκεφθέντεςΧώρες})
-  db.end
+  db.end;
 });
 
 // POST
+διακ.post('/add', async (req, res) => {
+  const νέαΧώρα = req.body.country;
+  const δείκτηςΧώρας = χώρες.findIndex(country => country.country_name === νέαΧώρα)
+  if (δείκτηςΧώρας === -1) {
+    alert('Η χώρα που ψάχνετε δεν υπάρχει με αυτό το όνομα.')
+  }
+  const κωδικόςΧώρας = χώρες[δείκτηςΧώρας].country_code
+  db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [`${κωδικόςΧώρας}`])
+  res.redirect('/')
+  db.end
+});
+
 // PUT
 // PATCH
 // DELETE
