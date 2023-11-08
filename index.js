dac31e8975941058d77abd6652198c6edd7a1465
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-11-08 18:24:17 +0100

    Update index.js
    Ενημερώστε το index.js για εντοπισμό σφαλμάτων
    
    Update index.js to catch errors

--- index.js
+++ index.js
@@ -19,37 +19,40 @@ const db = new pg.Client({
 db.connect();
 
 // Αρχικές Τιμές (Initial Values)
-let χώρες = []
+let χώρες = [];
 let επισκεφθείσεςΧώρες = [];
 let αριθμόςΧωρών = 0;
 let αριθμόςΕπισκεφθέντωνΧωρών = 0;
 
-
 // Δεδομένα (Data)
 // Λειτουργίες (Functions)
 const λήψηΧωρών = async (req, res, next) => {
-  const data = await db.query(`SELECT * FROM countries`)
-  χώρες = data.rows
-  αριθμόςΧωρών = data.rowCount
+  const data = await db.query(`SELECT * FROM countries`);
+  χώρες = data.rows;
+  αριθμόςΧωρών = data.rowCount;
   next();
-}
+};
 
 const λήψηΕπισκεφθέντωνΧωρών = async (req, res, next) => {
-  const data = await db.query(`SELECT * FROM visited_countries`)
+  const data = await db.query(`SELECT * FROM visited_countries`);
   αριθμόςΕπισκεφθέντωνΧωρών = data.rowCount;
   επισκεφθείσεςΧώρες = data.rows.map((row) => row.country_code);
   next();
-}
+};
 
 // Ενδιάμεσες Λειτουργίες (Middleware)
 διακ.use(bodyParser.urlencoded({ extended: true }));
 διακ.use(express.static('public'));
-διακ.use(λήψηΧωρών)
-διακ.use(λήψηΕπισκεφθέντωνΧωρών)
+διακ.use(λήψηΧωρών);
+διακ.use(λήψηΕπισκεφθέντωνΧωρών);
 
 // GET
 διακ.get('/', async (req, res) => {
-  console.log('Επισκεφθείσες Χώρες:', αριθμόςΕπισκεφθέντωνΧωρών, επισκεφθείσεςΧώρες);
+  console.log(
+    'Επισκεφθείσες Χώρες:',
+    αριθμόςΕπισκεφθέντωνΧωρών,
+    επισκεφθείσεςΧώρες
+  );
   res.render('index.ejs', {
     total: αριθμόςΕπισκεφθέντωνΧωρών,
     countries: επισκεφθείσεςΧώρες,
@@ -60,14 +63,41 @@ const λήψηΕπισκεφθέντωνΧωρών = async (req, res, next) => {
 // POST
 διακ.post('/add', async (req, res) => {
   const νέαΧώρα = req.body.country;
-  const δείκτηςΧώρας = χώρες.findIndex(country => country.country_name === νέαΧώρα)
-  if (δείκτηςΧώρας === -1) {
-    alert('Η χώρα που ψάχνετε δεν υπάρχει με αυτό το όνομα.')
+
+  try {
+
+    const αποτέλεσμα = await db.query(
+      `SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'`,
+      [`${νέαΧώρα.toLowerCase()}`]
+    )
+    const κωδικόςΧώρας = αποτέλεσμα.rows[0].country_code
+
+    // const δείκτηςΧώρας = χώρες.findIndex(
+    //   (country) => country.country_name === νέαΧώρα
+    // );
+    // const κωδικόςΧώρας = χώρες[δείκτηςΧώρας].country_code;
+
+    try {
+      await db.query(
+        'INSERT INTO visited_countries (country_code) VALUES ($1)',
+        [`${κωδικόςΧώρας}`]
+      );
+      res.redirect('/');
+    } catch (err) {
+      res.render('index.ejs', {
+        total: αριθμόςΕπισκεφθέντωνΧωρών,
+        countries: επισκεφθείσεςΧώρες,
+        error:
+          'The country you are trying to add has already been added. Please try again!',
+      });
+    }
+  } catch (err) {
+    res.render('index.ejs', {
+      total: αριθμόςΕπισκεφθέντωνΧωρών,
+      countries: επισκεφθείσεςΧώρες,
+      error: `The country you are trying to find doesn't exist with this name. Please try again!`,
+    });
   }
-  const κωδικόςΧώρας = χώρες[δείκτηςΧώρας].country_code
-  db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [`${κωδικόςΧώρας}`])
-  res.redirect('/')
-  db.end
 });
 
 // PUT
